6b932e2c9b407e6c2d47b82beb9f64b0b96d7973
RizkyGilang <rizkygilang839@gmail.com>
2019-09-18 16:38:08 +0700

    prak4

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,20 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText et1;
+    private TextView tv1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        et1 = findViewById(R.id.input_name);
+        tv1 = findViewById(R.id.text_output);
     }
 
+    public void handleSubmits(View view) {
+        String name = et1.getText().toString();
+        tv1.setText("Hello " + name);
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,18 +3,35 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static id.ac.polinema.intent.BundleActivity.AGE_KEY;
+import static id.ac.polinema.intent.BundleActivity.NAME_KEY;
+import static id.ac.polinema.intent.BundleActivity.USERNAME_KEY;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
-
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
         // TODO: bind here
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
+            String value1 = extras.get(USERNAME_KEY).toString();
+            String value2 = extras.get(NAME_KEY).toString();
+            String value3 = extras.get(AGE_KEY).toString();
+            usernameText.setText(value1);
+            nameText.setText(value2);
+            ageText.setText(value3);
             // TODO: display value here
         }
     }
